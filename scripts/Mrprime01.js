3082b3e5de52d8a08fc33900c4597e59dbac7c82
Mrprime01 <abdulsalamkabir99@gmail.com>
2020-06-04 18:14:49 +0100

    Modifying the code and also adding my email address

--- scripts/Mrprime01.js
+++ scripts/Mrprime01.js
@@ -1,2 +1,18 @@
 
-console.log("Hello World, this is Kabir Abdulsalam with HNGi7 ID HNG-05020 using JavaScripts for stage 2 task")
+//My Details
+let myDetails = {
+    fullName: "Kabir Abdulsalam",
+    internId: "HNG-05020",
+    language: "javascript",
+    email: "abdulsalamkabir99@gmail.com"
+};
+
+let cl = console.log;
+
+//Output
+const showText = () => {
+
+    cl(`Hello World, this is ${myDetails.fullName} with HNGi7 ID ${myDetails.internId} using ${myDetails.language} for stage 2 task  and ${myDetails.email}`);
+};
+
+showText();
